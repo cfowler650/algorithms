9eccef4e68058c835eda24b939e8bc3fd35e4ab7
cfowler650 <cfowler650@gmail.com>
2019-07-08 21:30:44 -0700

    Added more problems

--- searchalgs.js
+++ searchalgs.js
@@ -86,3 +86,53 @@ let BinaryInsert = function(root,val){
   return root
 }
 
+//recursive boolean return
+let BinarySearch = function(arr, target, start, end){
+  if (start>end) return false;
+
+  let midIDX = Math.floor((start+end)/2);
+  let midElement = arr[midIDX];
+
+  if (midElement === target) return true;
+
+  if(midElement > target){
+    return BinarySearch(arr, target, start, midIDX-1);
+  }else{
+    return BinarySearch(arr, target, midIDX+1,end);
+  }
+}
+
+//iterative index match return
+
+var BinarySearch = function (arr, target) {
+  var start = 0;
+  var end = arr.length - 1;
+
+  var middleIndex;
+
+  while (start <= end) {
+
+    if (start === end) {
+      middleIndex = start;
+    } else {
+      // end + start may overflow 
+      middleIndex = Math.floor((end - start) / 2) + start;
+    }
+
+    if (arr[middleIndex] === target) {
+      return middleIndex;
+    }
+
+    if (target < arr[middleIndex]) {
+      end = middleIndex - 1;
+    } else {
+      start = middleIndex + 1;
+    }
+  }
+
+  return -1;
+};
+
+
+
+
